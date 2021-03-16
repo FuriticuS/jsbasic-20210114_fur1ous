5ddb162d6f744ec8f74efd54e2b7c90d6c03573c
Fur1ouS <furiticus@gmail.com>
2021-03-16 18:41:52 +0300

    add tasks for six

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,43 @@
  */
 export default class UserTable {
   constructor(rows) {
+    // создание таблицы
+    this.elem = document.createElement('table');
+    this.tHead = document.createElement('thead');
+    this.tBody = document.createElement('tbody');
+    
+   this.tHead.innerHTML = `
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+    </tr>
+   `
+   this.elem.append(this.tHead);
+
+   for (let row of rows) {
+       let tbodyRow = document.createElement('tr');
+        tbodyRow.innerHTML = `
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button class = 'row-button'>X</button></td>
+        `
+        this.tBody.append(tbodyRow);
+   }
+   this.elem.append(this.tBody);
+   
+   // функции удаления строки
+   this.elem.onclick = this.removeRow;
   }
+
+  // функции удаления строки
+  removeRow = function(event) {
+        if(event.target.classList == 'row-button') {
+            event.target.closest('tr').remove();
+        }
+   };
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,45 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('card');
+
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.image = product.image;
+    this.id = product.id;
+
+    this.render();
+    this.addEventListeners();
   }
+
+  render() {
+    this.elem.innerHTML = `
+    <div class="card__top">
+        <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${this.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+`;
+  }
+
+  addEventListeners() {
+    this.elem.onclick = (event) => this.onClick(event);
+  }
+
+  onClick(event) {
+    this.elem.dispatchEvent(
+      new CustomEvent("product-add", { // имя события должно быть именно "product-add"
+        detail: this.id, // Уникальный идентификатора товара из объекта товара
+        bubbles: true // это событие всплывает - это понадобится в дальнейшем
+      })
+    );
+  }
+
 }
